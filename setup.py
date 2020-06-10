d914773834eef2f766af557a7c01718cb1e37bf2
alex-ber <toalexsmail@yahoo.com>
2020-06-10 09:29:39 +0300

    0.5.2.rc2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.5.2rc1'
+VERSION = '0.5.2rc2'
 from alexber.utils import UploadCommand
 NAME = 'alex_ber_utils'
 SHORT_NAME = 'utils'
