e10e4df50159744d6a78098b8fba3add0c5144c7
alex-ber <toalexsmail@yahoo.com>
2024-05-28 15:19:02 +0300

    v0.8.1a4

--- alexber/utils/pprint.py
+++ alexber/utils/pprint.py
@@ -9,6 +9,7 @@ you're importing pprint or any function from theire.
 import logging
 logger = logging.getLogger(__name__)
 
+import inspect
 import pprint
 from pprint import PrettyPrinter
 
@@ -25,18 +26,24 @@ class CustomPrettyPrinter(PrettyPrinter):
             'underscore_numbers': False
         }
 
+        # Get the parameters of PrettyPrinter.__init__
+        init_signature = inspect.signature(PrettyPrinter.__init__)
+        valid_params_d = init_signature.parameters #collections.OrderedDict
+
+        # Remove unsupported values from default_values
+        default_values = {k: v for k, v in default_values.items() if k in valid_params_d}
+
         # Map positional arguments to their corresponding keyword arguments
-        arg_names = list(default_values.keys())
-        arg_dict = dict(zip(arg_names, args))
+        args_names_l = list(default_values.keys())
+        args_d = dict(zip(args_names_l, args))
 
         # Check for conflicts between args and kwargs
-        for key, value in arg_dict.items():
+        for key, value in args_d.items():
             if key in kwargs:
                 raise ValueError(f"Parameter '{key}' is provided both as positional argument and keyword argument.")
 
         # Merge args and kwargs, giving priority to kwargs
-        combined_args = {**default_values, **arg_dict, **kwargs}
-
+        combined_args = {**default_values, **args_d, **kwargs}
         super().__init__(**combined_args)
 
 pprint.PrettyPrinter = CustomPrettyPrinter
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.8.1a3'
+VERSION = '0.8.1a4'
 from alexber.utils import UploadCommand
 NAME = 'alex_ber_utils'
 SHORT_NAME = 'utils'
