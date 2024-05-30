2a230a12b013af7a8101849f1c139088331d777b
alex-ber <toalexsmail@yahoo.com>
2024-05-31 01:14:43 +0300

    Changing version number is setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.9.1'
+VERSION = '0.10.0'
 from alexber.utils import UploadCommand
 NAME = 'alex_ber_utils'
 SHORT_NAME = 'utils'
