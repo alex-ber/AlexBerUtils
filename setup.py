23104727182bf4d5e9b7e686daa01b392850868b
alex-ber <toalexsmail@yahoo.com>
2024-07-14 17:35:50 +0300

    RLock dead-lock resistance is added (Conditional Variable on threading, and yileding control on asyncio).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.11.1b1'
+VERSION = '0.11.1b2'
 from alexber.utils import UploadCommand
 NAME = 'alex_ber_utils'
 SHORT_NAME = 'utils'
